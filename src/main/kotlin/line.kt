1c39100e5bf20e6f94c4adccef0a0e076cc7a71d
Viktoriiafedorcenko <123114307+Viktoriiafedorcenko@users.noreply.github.com>
2023-09-13 13:22:56 +0300

    Update line.kt

--- src/main/kotlin/line.kt
+++ src/main/kotlin/line.kt
@@ -2,7 +2,7 @@
      var startPoint:Point=Point(x1,y1) // начальная линия
      var endPoint:Point=Point(x2,y2) //  конец линии
      constructor(startPoint: Point,endPoint:Point): this (startPoint.x,startPoint.y,endPoint.x,endPoint.y)
-      // конструктор принимающий обьекты класса 
+      // конструктор принимающий координаты и втор  принимающий обьекты класса line
      open fun printlnInfo(){
          println("Line-точка1 : (${startPoint.x},${startPoint.y}) точка 2: (${endPoint.x},${endPoint.y})")
 
