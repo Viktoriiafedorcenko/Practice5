5a8d1f65386ecd9b2a110b3458ac0a88684f4c65
Viktoriiafedorcenko <123114307+Viktoriiafedorcenko@users.noreply.github.com>
2023-09-22 21:59:12 +0300

    Update coloredPoint.kt

--- src/main/kotlin/coloredPoint.kt
+++ src/main/kotlin/coloredPoint.kt
@@ -1,9 +1,10 @@
-class coloredPoint(x: Double,y: Double): Point( x, y   ) {
+class coloredPoint(x: Double,y: Double): Point( x, y   ) //наследование базового класса точка 
+{
     var color :String="Black" // значение по умолчанию
-        private set //установлени видимости   только для чтения  извне
+        private set //установлени видимости   только для чтения  извне пр  помощи сеттера
     constructor(x:Double,y: Double,color:String):this(x,y) //конструктор для  изменения цвета цвета
     {
-        this.color = color
+        this.color = color //вызов  первичного конструктора при помощи ключ слова  
     }
      fun printlInfo() {
         println(" Цвет : $color")
